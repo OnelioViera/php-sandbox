3adcfca856f38eb2d2a9af10f05766d1d395c12a
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-11 21:09:17 -0600

    Perform implicit conversion for addition operations in index.php

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 $number1 = 5;
 $number2 = 10;
 $number3 = '20';
+$fruit = 'apple';
+$bool1 = true;
+$bool2 = false;
+$null = null;
 
 // Implicit conversion
-$result = $number1 + $number2;
+$result = $number1 + $number2; // int
 $result = $number1 + $number3; // int (string to int)
+$result = $number3 + $number3; // int (string to int)
+$result = $number1 . $number2; // string (int to string)
+// $result = $fruit + $number2; // Error (string to int)
+$result = $number1 + $bool1; // int (bool to int)
+$result = $number1 + $bool2; // int (bool to int)
+$result = $number1 + $null; // int (null to int)
 
 var_dump($result);
