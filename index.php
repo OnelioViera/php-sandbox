f0be66160b27a7b2a72521c6ed0e0e4fb3a5a8e3
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-11 20:17:18 -0600

    feat: Perform addition operation with implicit conversion
    Perform addition operation with implicit conversion in index.php
    
    - Add two numbers and store the result in a variable
    - Demonstrate implicit conversion by adding a number and a string

--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <?php
-  // Single Line Comment
-  /*
-    Multi Line Comment
-  */
-  echo 'Hello World';
-  echo '<br>';
-  print 'Hello from Print';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+$number1 = 5;
+$number2 = 10;
+$number3 = '20';
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title><?= 'Learn PHP From Scratch' ?></title>
-</head>
+// Implicit conversion
+$result = $number1 + $number2;
+$result = $number1 + $number3; // int (string to int)
 
-<body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch' ?></h1>
-        </div>
-    </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome To The Course' ?></h2>
-            <?php echo '<p>In this course, you will learn the fundamentals of the PHP language</p>'; ?>
-        </div>
-    </div>
-</body>
-
-</html>
+var_dump($result);
